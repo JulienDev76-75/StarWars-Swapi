aeab7653a415239cc3661ee09befa2f8c5a96222
JulienDev76-75 <julien.coart@gmail.com>
2021-06-13 23:51:56 +0200

    components done, props/state done

--- src/App.js
+++ src/App.js
@@ -1,44 +1,65 @@
-import './App.css';
-import Header from './component/layout/Header';
-import Footer from './component/layout/Footer';
-import Nav from './component/layout/Nav';
-import Home from './pages/home/Home';
-import Search from './pages/search/Search';
 import React from "react";
+
+import './App.css';
+import Header from "./components/layout/header/Header";
+import Footer from "./components/layout/footer/Footer";
+import Nav from "./components/layout/nav/Nav";
+import Research from "./components/layout/nav/research/Research";
+import Explore from "./components/layout/section_explore/explore/Explore";
+import Films from "./components/layout/section_explore/list/films/Films.js";
+import People from "./components/layout/section_explore/list/people/People";
+import Planets from "./components/layout/section_explore/list/planets/Planets";
+import Species from "./components/layout/section_explore/list/species/Species";
+import Vehicles from "./components/layout/section_explore/list/vehicles/Vehicles";
+import Starships from "./components/layout/section_explore/list/starships/Starships";
 
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
+  Route
+  
 } from "react-router-dom";
 
 function App() {
   return (
-      <div>     
-        <div style={{ 
-          backgroundImage: `url(${process.env.PUBLIC_URL + './img/starwars2.jpg'})`,
-          backgroundRepeat: 'no-repeat',
-        }}> 
-      </div>
+    <div>
       <Header />
-      <Router>
+      <Router>        
         <Nav />
-        <main className="container my-5">
+        <main className="container m-5">   
           <Switch>
-            <Route path="/star">
-              <Home />
+            <Route path="/home">
+              <Explore />
             </Route>
-            <Route path="/search">
-              <Search />
+            <Route path="/films">
+              <Films />
+            </Route>
+            <Route path="/people">
+              <People />
+            </Route>
+            <Route path="/planets">
+              <Planets />
+            </Route>
+            <Route path="/species">
+              <Species />
+            </Route>
+            <Route path="/vehicles">
+              <Vehicles />
+            </Route>
+            <Route path="/starships">
+              <Starships />
+            </Route>           
+            <Route path="/research">
+              <Research />
             </Route>
             <Route path="/">
-              <Home />
-            </Route>
+              <Explore />
+            </Route>                      
           </Switch>
         </main>
       </Router>
       <Footer />
-    </div>
+   </div>
   );
 }
 
